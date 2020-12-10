107182ca16b4036e0fe955bbefe55e82ad505db3
Theo Törnvall Hjortenmark <theo.tornvallhjorte@learnet.se>
2020-12-10 11:10:38 +0100

    first commit

---
+++ battleship/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello player, tell me your username");
+
+            string username = Console.Readline();
+        }
+    }
+}
